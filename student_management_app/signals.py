ca416ccacdf1b3c4f7c5dfdeb90aba63500446c4
Wendy <wangjianan135@gmail.com>
2022-12-05 01:12:30 +0800

    fix user email problems

--- student_management_app/signals.py
+++ student_management_app/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Student, Teacher
@@ -8,17 +8,28 @@ from .models import Student, Teacher
 def add_to_auth_user(sender, instance, created, **kwargs):
     print("add to auth user")
     if created:
-        User.objects.create_user(username=instance.S_number, password='Wang@123')
+        User.objects.create_user(username=instance.S_number, email=instance.S_email, password='Wang@123')
         #如果提供邮箱
+
+
+@receiver(post_delete, sender=Student)
+def add_to_auth_user(sender, instance, **kwargs):
+    u = User.objects.get(username=instance.S_number)
+    u.delete()
 
 
 @receiver(post_save, sender=Teacher)
 def add_to_auth_user(sender, instance, created, **kwargs):
     print("add to auth user")
     if created:
-        User.objects.create_user(username=instance.T_number, password='Wang@123')
+        User.objects.create_user(username=instance.T_number, email=instance.T_email, password='Wang@123')
         #如果提供邮箱
 
+
+@receiver(post_delete, sender=Teacher)
+def add_to_auth_user(sender, instance, **kwargs):
+    u = User.objects.get(username=instance.T_number)
+    u.delete()
 
 # @receiver(post_save, sender=Student)
 # def save_to_auth_user(sender, instance, **kwargs):
